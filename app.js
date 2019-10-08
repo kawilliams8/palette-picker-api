1f803705570f231608d8b918167abe781ea0c002
Katie Williams <kawilliams8@gmail.com>
2019-10-08 17:35:05 -0600

    Add happy and sad path tests for GET /projects, /palettes, /projects/:id, /palettes/:id and endpoints to pass tests. Install regeneratorRuntime to handle async errors.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const regeneratorRuntime = require("regenerator-runtime");
 
 const environment = process.env.NODE_ENV || 'development'; 
 const configuration = require('./knexfile')[environment];
@@ -7,8 +8,36 @@ const database = require('knex')(configuration);
 
 app.use(express.json());
 
-app.get('/projects', (request, response) => {
-  response.send('Looks like the server is running!');
+app.get("/", (request, response) => {
+  response.send("We're going to test all the routes!");
+});
+
+app.get('/projects', async (request, response) => {
+  const projects = await database("projects").select();
+  return response.status(200).json(projects);
+});
+
+app.get("/palettes", async (request, response) => {
+  const palettes = await database("palettes").select();
+  return response.status(200).json(palettes);
+});
+
+app.get("/projects/:id", async (request, response) => {
+  const project = await database("projects").where("id", request.params.id).first();
+  if (project) {
+    return response.status(200).json(project);
+  } else {
+    return response.status(404).json({error: "No project found with that id"});
+  }
+});
+
+app.get("/palettes/:id", async (request, response) => {
+  const palette = await database("palettes").where("id", request.params.id).first();
+  if (palette) {
+    return response.status(200).json(palette);
+  } else {
+    return response.status(404).json({error: "No palette found with that id"});
+  }
 });
 
 module.exports = app;
--- app.test.js
+++ app.test.js
@@ -1,10 +1,10 @@
-const request = require("supertest");
-const app = require("./app");
+const request = require('supertest');
+const app = require('./app');
 const regeneratorRuntime = require('regenerator-runtime');
 
-const environment = process.env.NODE_ENV || "development";
-const configuration = require("./knexfile")[environment];
-const database = require("knex")(configuration);
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 describe('Server', () => {
 
@@ -12,9 +12,9 @@ describe('Server', () => {
     await database.seed.run();
   });
 
-  describe("init", () => {
-    it("should return a 200 status", async () => {
-      const res = await request(app).get("/");
+  describe('init', () => {
+    it('should return a 200 status', async () => {
+      const res = await request(app).get('/');
       expect(res.status).toBe(200);
     });
   });
@@ -22,10 +22,62 @@ describe('Server', () => {
   describe('GET /projects', () => {
     it('should return a 200 and all of the projects', async () => {
       const expectedProjects = await database('projects').select();
-      const response = await request(app).get("/projects");
+      const response = await request(app).get('/projects');
       const projects = response.body;
       expect(response.status).toBe(200);
-      expect(projects).toEqual(expectedProjects);
+      expect(JSON.stringify(projects)).toEqual(JSON.stringify(expectedProjects));
+    });
+  });
+
+  describe('GET /palettes', () => {
+    it('should return a 200 and all of the palettes', async () => {
+      const expectedPalettes = await database('palettes').select();
+      const response = await request(app).get('/palettes');
+      const palettes = response.body;
+      expect(response.status).toBe(200);
+      expect(JSON.stringify(palettes)).toEqual(JSON.stringify(expectedPalettes));
+    });
+  });
+
+  describe("GET /projects/:id", () => {
+    it("should return a 200 and one matching project", async () => {
+      const expectedProject = await database("projects").first();
+      const id = expectedProject.id;
+
+      const response = await request(app).get(`/projects/${id}`);
+      const result = response.body;
+
+      expect(response.status).toBe(200);
+      expect(JSON.stringify(result)).toEqual(JSON.stringify(expectedProject));
+    });
+
+    it('should return a 404 and the error "No project found with that id"', async () => {
+      const invalidID = -1;
+      const response = await request(app).get(`/projects/${invalidID}`);
+
+      expect(response.status).toBe(404);
+      expect(response.body.error).toEqual("No project found with that id");
+    });
+  });
+
+  describe("GET /palettes/:id", () => {
+    it("should return a 200 and one matching palette", async () => {
+      const expectedPalette = await database("palettes").first();
+      const id = expectedPalette.id;
+
+      const response = await request(app).get(`/palettes/${id}`);
+      const result = response.body;
+
+      expect(response.status).toBe(200);
+      expect(JSON.stringify(result)).toEqual(JSON.stringify(expectedPalette));
+    });
+
+    it('should return a 404 and the error "No palette found with that id"', async () => {
+      const invalidID = -1;
+      const response = await request(app).get(`/palettes/${invalidID}`);
+
+      expect(response.status).toBe(404);
+      expect(response.body.error).toEqual("No palette found with that id");
     });
   });
 })
