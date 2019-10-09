65267ed92a47dc87eda597991ef584ec004492b8
davidagitlen <davidagitlen@gmail.com>
2019-10-09 15:46:55 -0600

    Add tests for post /projects happy and sad paths, and endpoint to pass tests

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const regeneratorRuntime = require("regenerator-runtime");
+const regeneratorRuntime = require('regenerator-runtime');
 
 const environment = process.env.NODE_ENV || 'development'; 
 const configuration = require('./knexfile')[environment];
@@ -8,36 +8,66 @@ const database = require('knex')(configuration);
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  response.send("We're going to test all the routes!");
+app.get('/', (request, response) => {
+  response.send('We\'re going to test all the routes!');
 });
 
 app.get('/projects', async (request, response) => {
-  const projects = await database("projects").select();
+  const projects = await database('projects').select();
   return response.status(200).json(projects);
 });
 
-app.get("/palettes", async (request, response) => {
-  const palettes = await database("palettes").select();
+app.get('/palettes', async (request, response) => {
+  const palettes = await database('palettes').select();
   return response.status(200).json(palettes);
 });
 
-app.get("/projects/:id", async (request, response) => {
-  const project = await database("projects").where("id", request.params.id).first();
+app.get('/projects/:id', async (request, response) => {
+  const project = await database('projects').where('id', request.params.id).first();
   if (project) {
     return response.status(200).json(project);
   } else {
-    return response.status(404).json({error: "No project found with that id"});
+    return response.status(404).json({error: 'No project found with that id'});
   }
 });
 
-app.get("/palettes/:id", async (request, response) => {
-  const palette = await database("palettes").where("id", request.params.id).first();
+app.get('/palettes/:id', async (request, response) => {
+  const palette = await database('palettes').where('id', request.params.id).first();
   if (palette) {
     return response.status(200).json(palette);
   } else {
-    return response.status(404).json({error: "No palette found with that id"});
+    return response.status(404).json({error: 'No palette found with that id'});
   }
 });
+
+app.post('/projects', async (request, response) => {
+  const project = request.body;
+  const duplicateProject = await database('projects').where('project', 'like', `%${project.project}%`);
+  if (!project.project) {
+    return response 
+      .status(422)
+      .send({
+        error: `Expected format: {
+          project: <String>
+        }. You are missing a "project" property.`})
+  } else if (duplicateProject.length) {
+    return response
+      .status(400)
+      .send({
+        error: 'Duplicate project name. Please choose another name.'
+      });
+  }
+
+  const insertedProject = await database('projects').insert(project, 'id');
+
+  if (insertedProject.length) {
+    return response.status(201).json({ id: insertedProject[0] })
+  } else {
+    return response.status(422).json({ error: 'Failed to post project!'})
+  }
+
+});
+
+
 
 module.exports = app;
--- app.test.js
+++ app.test.js
@@ -39,9 +39,9 @@ describe('Server', () => {
     });
   });
 
-  describe("GET /projects/:id", () => {
-    it("should return a 200 and one matching project", async () => {
-      const expectedProject = await database("projects").first();
+  describe('GET /projects/:id', () => {
+    it('should return a 200 and one matching project', async () => {
+      const expectedProject = await database('projects').first();
       const id = expectedProject.id;
 
       const response = await request(app).get(`/projects/${id}`);
@@ -56,13 +56,13 @@ describe('Server', () => {
       const response = await request(app).get(`/projects/${invalidID}`);
 
       expect(response.status).toBe(404);
-      expect(response.body.error).toEqual("No project found with that id");
+      expect(response.body.error).toEqual('No project found with that id');
     });
   });
 
-  describe("GET /palettes/:id", () => {
-    it("should return a 200 and one matching palette", async () => {
-      const expectedPalette = await database("palettes").first();
+  describe('GET /palettes/:id', () => {
+    it('should return a 200 and one matching palette', async () => {
+      const expectedPalette = await database('palettes').first();
       const id = expectedPalette.id;
 
       const response = await request(app).get(`/palettes/${id}`);
@@ -77,7 +77,41 @@ describe('Server', () => {
       const response = await request(app).get(`/palettes/${invalidID}`);
 
       expect(response.status).toBe(404);
-      expect(response.body.error).toEqual("No palette found with that id");
+      expect(response.body.error).toEqual('No palette found with that id');
     });
   });
+
+  describe('POST /projects', () => {
+    it('should successfully post a new project to the db', async () => {
+      const newProject = { project: "TestPost"}
+      const res = await request(app).post('/projects').send(newProject);
+      const project = await database('projects').where('project', 'like',  `%${newProject.project}%`).first();
+
+      expect(res.status).toBe(201);
+      expect(project.project).toEqual(newProject.project);
+    });
+
+    it('should return a 422 and a message "Request body is missing a parameter"', async () => {
+      const newProject = { fake: 'fake' }
+
+      const res = await request(app).post('/projects').send(newProject); 
+
+      expect(res.status).toBe(422);
+      expect(res.body.error).toEqual(
+        `Expected format: {
+          project: <String>
+        }. You are missing a "project" property.`
+      );
+    });
+
+    it('should return a 400 and a message "Duplicate project name. Please choose another name."', async () => {
+      const newProject = { project: 'Project_One' };
+
+      const res = await request(app).post('/projects').send(newProject);
+
+      expect(res.status).toBe(400);
+      expect(res.body.error).toEqual('Duplicate project name. Please choose another name.')
+    });
+  });
+
 })
